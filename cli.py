d8df621a02ceaa48b09a91c226b44d333b05f5d9
Sean Martin <seankieran.m@hotmail.com>
2022-03-21 14:49:20 +0000

    Template

--- cli.py
+++ cli.py
@@ -1,10 +1,12 @@
 from openneurodata.your_module import TemplateClass
 
+
 def main():
     first = TemplateClass(5, 2)
     second = TemplateClass(10, 4)
 
     print("Wow I got a result of {} adding these".format(first + second))
+
 
 if __name__ == "__main__":
     main()
