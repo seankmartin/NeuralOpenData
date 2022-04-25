a8fe0a2d3d72e94c9fa15a881b285f09d7220a10
Sean Martin <seankieran.m@hotmail.com>
2022-04-25 16:46:37 +0100

    Working on analyse ophys example

---
+++ openneurodata/analyse_ophys.py
@@ -0,0 +1,15 @@
+def summarise_single_session(allen_dataset):
+
+    ## Summary in print
+    print(
+        f"-----------Working on image plane {allen_dataset.ophys_experiment_id}"
+        f"session {allen_dataset.ophys_session_id}------------"
+    )
+    print(f"\tThis experiment has metadata {allen_dataset.metadata}")
+    cell_specimen_table = allen_dataset.cell_specimen_table
+    print(
+        f"\tThere are {len(cell_specimen_table)} cells"
+        f"in this session with IDS {cell_specimen_table['cell_specimen_id']}"
+    )
+
+    ## Plotting the
